ff6623a59333bd0344cc71157131006ece4ccc75
Diovani Bernardi da Motta <diovanimotta@gmail.com>
2015-05-17 16:11:39 -0300

    Commit Parcial
    Implementação do mecanismo de transação SOAP.

--- soap-annotation/src/org/soap/diovani/motta/annotations/SoapEntity.java
+++ soap-annotation/src/org/soap/diovani/motta/annotations/SoapEntity.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface SoapObject {
+public @interface SoapEntity {
 	
 	String namespace();
 	
